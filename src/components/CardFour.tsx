ad02610f82ec54edac511b8b42f5246e6979cdfd
rohit <rohit@example.com>
2024-01-11 18:30:52 +0530

    update the code

--- src/components/CardFour.tsx
+++ src/components/CardFour.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 const CardFour = () => {
   return (
     <div className="rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
@@ -25,29 +26,17 @@ const CardFour = () => {
         </svg>
       </div>
 
-      <div className="mt-4 flex items-end justify-between">
+      <div className="mt-4 flex items-end justify-around">
         <div>
           <h4 className="text-title-md font-bold text-black dark:text-white">
-            3.456
+            12
           </h4>
-          <span className="text-sm font-medium">Total Users</span>
+          <span className="text-sm font-medium ">New users in this week</span>
         </div>
 
-        <span className="flex items-center gap-1 text-sm font-medium text-meta-5">
-          0.95%
-          <svg
-            className="fill-meta-5"
-            width="10"
-            height="11"
-            viewBox="0 0 10 11"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M5.64284 7.69237L9.09102 4.33987L10 5.22362L5 10.0849L-8.98488e-07 5.22362L0.908973 4.33987L4.35716 7.69237L4.35716 0.0848701L5.64284 0.0848704L5.64284 7.69237Z"
-              fill=""
-            />
-          </svg>
+        <span className="flex items-center gap-1 text-sm font-medium text-[#3C50E0] ">
+          <Link to="#">view all</Link>
+
         </span>
       </div>
     </div>
